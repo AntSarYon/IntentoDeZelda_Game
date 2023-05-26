c28384060b8b18b2876a8033e604d858c9314478
EmilioBerrocal <20180235@aloe.ulima.edu.pe>
2023-05-25 20:58:05 -0500

    Commit

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -7,6 +7,6 @@ public class StartGame : MonoBehaviour
     public void EmpezarJuego()
     {
         //Cargamos la Escena de MainScene
-        SceneManager.LoadScene("PruebasANTONIO");
+        SceneManager.LoadScene("PruebasEmilio");
     }
 }
